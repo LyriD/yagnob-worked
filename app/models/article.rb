74d9dc053e4872b3da075fe7c3e90e54a8e16e71
lyrs <lyrs@me.com>
2014-09-30 14:24:45 +0400

    глагне

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,6 @@ class Article < ActiveRecord::Base
   has_many :spree_images, -> { where viewable_type: 'Article' }, :class_name => 'Spree::Image', foreign_key: :viewable_id, dependent: :destroy
   accepts_nested_attributes_for :spree_images, :reject_if => lambda { |a| a[:attachment].blank? }, allow_destroy: true
 
+  default_scope { order(:created_at => :desc) }
+
 end
