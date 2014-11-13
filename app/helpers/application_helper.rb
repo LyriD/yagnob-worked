7f3e444c9a8d4ff9a41551e53eab77a5434ddfbd
lyrs <lyrs@me.com>
2014-11-13 13:30:30 +0300

    почищена корзина, кнопка выхода, добавлено проверок в контроллеры, изменен процесс формирования заказа, мелкие фиксы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+
+  def true_var(product)
+    if spree_current_user
+    if spree_current_user.price_kind == -1
+      out = product.variants
+      out << product.master if out.blank?
+    elsif !spree_current_user.price_kind.blank?
+          product.variants.each do |variant|
+            if variant.option_values.first.name.to_i == spree_current_user.price_kind.to_i
+              out = variant
+            end
+          end
+    end
+    end
+
+    return out
+  end
+
 end
