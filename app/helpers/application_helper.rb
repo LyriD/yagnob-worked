1a78b8bfb7de5d101197a6b453f66ffdd5a0309f
lyrs <lyrs@me.com>
2015-03-10 20:55:07 +0300

    незалогиненым не показывать цены

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,6 @@ module ApplicationHelper
   def true_var(product)
     out = []
     if spree_current_user
-
-
     if spree_current_user.price_kind == -1
           out << product.variants
           out << product.master if out.blank?
@@ -16,14 +14,8 @@ module ApplicationHelper
             end
           end
     end
-
-
     else
-      product.variants.each do |variant|
-        if variant.option_values.first.name.to_i == 1
-          out << variant
-        end
-      end
+      out = []
     end
     o = (out.first.class == Spree::Variant)? out : out.first
     return o
